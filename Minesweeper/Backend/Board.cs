de3d749a187eb616a17da97e44f8a9ed3a304d47
DESKTOP-2T0BL0M\Louis <louis_luu_lim@hotmail.com>
2022-03-07 20:30:45 -0500

    Initialize board and mines

--- Minesweeper/Backend/Board.cs
+++ Minesweeper/Backend/Board.cs
@@ -1,10 +1,13 @@
-﻿namespace Minesweeper.Backend
+﻿using System;
+using System.Windows;
+
+namespace Minesweeper.Backend
 {
     public class Board
     {
         int _height;
         int _width;
-        Tile[] _tiles;
+        Tile[,] _tiles;
 
         public Board(int height, int width)
         {
@@ -13,16 +16,35 @@
             _tiles = InitializeTiles(height, width);          
         }
 
-        private Tile[] InitializeTiles(int height, int width)
+        private Tile[,] InitializeTiles(int height, int width)
         {
-            var tiles = new Tile[height * width];
+            var tiles = new Tile[height, width];
 
-            for (int i = 0; i < tiles.Length; i++)
+            for (int i = 0; i < height; i++)
             {
-                tiles[i] = new Tile();
+                for (int j = 0; j < width; j++)
+                {
+                    tiles[i, j] = new Tile();
+                }
             }
 
             return tiles;
+        }
+
+        public void InitializeMines(int x, int y, int minesCount)
+        {
+            _tiles[y, x] = new MineTile();
+            minesCount--;
+
+            var randomGenerator = new Random();
+
+            for (int i = minesCount - 1; i >= 0; i--)
+            {
+                x = randomGenerator.Next(_width);
+                y = randomGenerator.Next(_height);
+
+                _tiles[y, x] = new MineTile();
+            }
         }
     }
 }
